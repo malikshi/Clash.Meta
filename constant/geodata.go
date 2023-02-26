1c02098ffbfa8e382d2c6f9c47cc110d7d9a0441
Malik Al <kidznightserver@gmail.com>
2023-02-26 21:25:28 +0700

    via jsdelivr

--- constant/geodata.go
+++ constant/geodata.go
@@ -2,7 +2,7 @@ package constant
 
 var (
 	GeodataMode bool
-	GeoIpUrl    = "https://ghproxy.com/https://github.com/malikshi/v2ray-rules-dat/releases/latest/download/geoip.dat"
-	MmdbUrl     = "https://ghproxy.com/https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb"
-	GeoSiteUrl  = "https://ghproxy.com/https://github.com/malikshi/v2ray-rules-dat/releases/latest/download/geosite.dat"
+	GeoIpUrl    = "https://cdn.jsdelivr.net/gh/malikshi/v2ray-rules-dat@release/geoip.dat"
+	MmdbUrl     = "https://cdn.jsdelivr.net/gh/Loyalsoldier/geoip@release/Country.mmdb"
+	GeoSiteUrl  = "https://cdn.jsdelivr.net/gh/malikshi/v2ray-rules-dat@release/geosite.dat"
 )
