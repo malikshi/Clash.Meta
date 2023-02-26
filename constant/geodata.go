63de9e7ad13b8dfcd5a5b4d5e7dc826464086b68
Malik Al <kidznightserver@gmail.com>
2023-02-26 13:49:21 +0700

    update geodata

--- constant/geodata.go
+++ constant/geodata.go
@@ -2,7 +2,7 @@ package constant
 
 var (
 	GeodataMode bool
-	GeoIpUrl    = "https://ghproxy.com/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat"
+	GeoIpUrl    = "https://ghproxy.com/https://github.com/malikshi/v2ray-rules-dat/releases/latest/download/geoip.dat"
 	MmdbUrl     = "https://ghproxy.com/https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb"
-	GeoSiteUrl  = "https://ghproxy.com/https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat"
+	GeoSiteUrl  = "https://ghproxy.com/https://github.com/malikshi/v2ray-rules-dat/releases/latest/download/geosite.dat"
 )
